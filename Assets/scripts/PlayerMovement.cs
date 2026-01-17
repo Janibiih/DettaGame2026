717eadfa623ea4f3521abfb7a5fe6562aa08b283
daniel9925 <daniel129936@gmail.com>
2026-01-17 15:31:44 +0100

    Movement angepasst  und Items sammeln

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -7,18 +7,32 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     public Rigidbody2D rb;
     public float moveSpeed = 5f;
-    public Vector2 moveInput; 
+    public Vector2 moveInput;
+    public int itemCount;
 
-    private Object items; 
+    [SerializeField] float rotateSpeed = 10f;
+    [SerializeField] Transform spriteTransform; 
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        itemCount = 0; 
     }
 
     private void Update()
     {
         rb.linearVelocity = moveInput * moveSpeed;
+
+        float targetAngle = 0f; 
+
+        if (moveInput != Vector2.zero)
+        {
+            targetAngle = Mathf.Atan2(moveInput.y, moveInput.x) * Mathf.Rad2Deg - 90f;
+            Quaternion targetRot = Quaternion.Euler(0, 0, targetAngle);
+            spriteTransform.rotation = Quaternion.Lerp(spriteTransform.rotation, targetRot, rotateSpeed * Time.deltaTime);
+        }
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -29,7 +43,12 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.tag == "Object")
+        {
+            Destroy(collision.gameObject); 
+            itemCount ++;
+            Debug.Log("Items: " + itemCount);
+        }
     }
 
 
