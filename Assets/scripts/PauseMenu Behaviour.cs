5e5e2e7434a7196390cb54ed7df577fe6de034c4
Janibiih <Jan.Oberst@mailbox.org>
2026-01-17 19:34:44 +0100

    Pause Menu finished + ButtonSprite change

--- Assets/scripts/PauseMenu Behaviour.cs
+++ Assets/scripts/PauseMenu Behaviour.cs
@@ -1,4 +1,4 @@
-using Unity.VisualScripting;
+using UnityEditor.Overlays;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,27 +6,38 @@ public class PauseMenuBehaviour : MonoBehaviour
 {
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
+
+    private PlayerInput playerInput;
     private InputAction pauseAction;
 
-    void Update()
+    void Awake()
     {
-        if (Keyboard.current.pKey.wasPressedThisFrame)
-        {
-            if (GameIsPaused)
-            {
-               
-                Resume();
-            }
-            else
-            {
-                Pause();
-            }
-        }
+        playerInput = GetComponent<PlayerInput>();
+        pauseAction = playerInput.actions["Pause"];
+    }
+
+    void OnEnable()
+    {
+        pauseAction.performed += OnPause;
+    }
+
+    void OnDisable()
+    {
+        pauseAction.performed -= OnPause;
+    }
+
+    private void OnPause(InputAction.CallbackContext context)
+    {
+        if (GameIsPaused)
+            Resume();
+        else
+            Pause();
     }
 
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
+        
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
@@ -34,6 +45,7 @@ public class PauseMenuBehaviour : MonoBehaviour
     void Pause()
     {
         pauseMenuUI.SetActive(true);
+   
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
